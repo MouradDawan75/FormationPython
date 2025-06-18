14d1dc84d415ac58c639ccb623057ed9b11a946d
Mourad <mmahrane@dawan.fr>
2025-06-18 14:28:58 +0200

    Jour3-Fin

---
+++ 18-Demo_Streamlit/Intro.py
@@ -0,0 +1,20 @@
+# pip install streamlit
+import streamlit as st
+
+st.title('😁​ Introduction à Streamlit 😁​')
+
+st.subheader('Installtion')
+
+st.write("""
+Installation: \n
+pip install streamlit
+""")
+
+st.subheader('Exécution:')
+
+st.write("""
+Commande à exécuter dans le terminal:\n
+streamlit run chemin_fichier.py
+""")
+
+# streamlit run 18-Demo_Streamlit\\Intro.py
